6d75513a61a9b8338cd72033d3ed244acd8020f7
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-05-24 22:23:21 +0100

    CREATE: Add post interface for service

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,30 @@
 import { UserInput } from "./users/user.validator";
-import type { User } from "@prisma/client";
+import { type User, type Post, Prisma, PostDetails } from "@prisma/client";
+import type { PostInput, PostQuery } from "./posts/post.validator";
 
 export interface IUserService {
   addUser: (user: UserInput) => Promise<void>;
   findUserByEmail: (email: string) => Promise<User | null>;
   findUserById: (id: string) => Promise<User | null>;
   findUserByUsername: (username: string) => Promise<User | null>;
-  updateUserProfile: (
-    id: string,
-    data: Partial<User>
-  ) => Promise<User | null>;
+  updateUserProfile: (id: string, data: Partial<User>) => Promise<User | null>;
 }
 
+export interface IPostService {
+  getAllPosts: (query?: PostQuery) => Promise<Post[]>;
+  getSinglePost: (id: string) => Promise<Post | null>;
+  createPost: (
+    userId: string,
+    postData: PostInput["postData"],
+    postDetails: PostDetails
+  ) => Promise<Post>;
+  updatePost: (
+    postId: string,
+    userId: string,
+    postData: PostInput["postData"]
+  ) => Promise<Post>;
+  deletePost: (postId: string, userId: string) => Promise<Post>;
+}
 
 declare global {
   namespace Express {
